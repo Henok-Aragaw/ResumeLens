028cbfd451b6d003706af7e58aaef4794b37f336
henok <Henok@ablazelabs.com>
2026-01-05 17:12:23 +0300

    fix nitro issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,20 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import { nitro } from 'nitro/vite'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
     devtools(),
-    netlify(),
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
+
     tanstackStart(),
+
+    nitro(),
+
     viteReact(),
   ],
 })
-
-export default config
